7995e0c9c331c2995ca3b7e2d2245e9166ca0f8b
Matt <matt@timmy.home>
2023-01-13 20:51:46 +0100

    Cambios de prueba

--- Javascript/calc.js
+++ Javascript/calc.js
@@ -1,4 +1,4 @@
-// CALCULADORA MUY PERO MUY FALOPA
+// CALCULADORA MUY PERO MUY FALOPA!
 
 let numeroUno = Number(prompt('Ingresa un numero: '));
 
@@ -19,3 +19,5 @@ if (!isNaN(numeroUno)) {
                 }
         }
 }
+
+console.log('Hola viteh')
