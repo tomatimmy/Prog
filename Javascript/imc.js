9591e9dfa6b75dbcc0178ceaf07c7b50380803ba
tomatimmy <vanarellimatias@gmail.com>
2023-01-18 20:26:59 +0100

    Modificaciones varias

--- Javascript/imc.js
+++ Javascript/imc.js
@@ -1,21 +1,36 @@
-let peso = parseInt(prompt('Ingrese su peso en kilogramos: '));
-let altura = parseInt(prompt('Ingrese su altura en centimetros: '));
+let peso = parseInt(prompt('Ingrese su peso en kilogramos: (ingrese 0 para omitir)'));
+while (isNaN(peso)) {
+    peso = parseInt(prompt('Ingrese su peso en kilogramos: (ingrese 0 para omitir)'));
+    if (peso === 0) {
+        break;
+    }
+}
 
-let imc = (peso / (altura * altura)) * 10000; 
+let altura = parseInt(prompt('Ingrese su altura en centimetros: (ingrese 0 para omitir)'));
+while (isNaN(altura)) {
+    altura = parseInt(prompt('Ingrese su altura en centimetros: (ingrese 0 para omitir)'));
+    if (altura === 0) {
+        break;
+    }
+}
 
-// Calculadora actualizada con lo aprendido hoy (condicionales)
 
+let imc = (peso / (altura * altura)) * 10000;
 let teFaltaGuiso = (imc < 18.5)
 let estasJamon = (imc >= 18.5 && imc < 24.9)
 let tenesDeDondeAgarrar = (imc >= 24.9 && imc < 29.9)
-// let tusHuesosSonGrandes = (imc >= 29.9) esta la podemos suplantar por el 'else'
+let tusHuesosSonGrandes = (imc >= 29.9)
 
-if (teFaltaGuiso) {
-    console.log('Tu IMC es ' + imc + '. Te falta Guiso.');
+if (peso === 0 && altura === 0) {
+    alert('Me hiciste trabajar al pedo, no servís para nada')
+} else if (peso === 0 || altura === 0) {
+    alert('Omitiste un dato, corki.');
+} else if (teFaltaGuiso) {
+    alert('Tu IMC es ' + imc + '. Te falta Guiso.');
 } else if (estasJamon) {
-    console.log('Tu IMC es ' + imc + '. Estas Jamon.');
+    alert('Tu IMC es ' + imc + '. Estas Jamon.');
 } else if (tenesDeDondeAgarrar) {
-    console.log('Tu IMC es ' + imc + '. Pa que tengas de donde agarrar.');
+    alert('Tu IMC es ' + imc + '. Pa que tengas de donde agarrar.');
 } else {
-    console.log('Tu IMC es ' + imc + '. NO ESTAS GORDO, TENES HUESOS GRANDES.');
+    alert('Tu IMC es ' + imc + '. NO ESTAS GORDO, TENES HUESOS GRANDES.');
 }
